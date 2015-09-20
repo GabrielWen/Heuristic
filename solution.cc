4af78477728d6f7f51214bb6efd96a30efc4cfea
GabrielWen <gabriel.weng@gmail.com>
2015-09-20 18:06:20 -0400

    Q2 migrated

--- solution.cc
+++ solution.cc
@@ -141,8 +141,71 @@ float exactScore(set<int> denoms, void* obj) {
 }
 
 float exchangeScore(set<int> denoms, void* obj) {
+  if (!denoms.size()) {
+    return 0.f;
+  }
   Solution *ptr = (Solution*) obj;
-  return 3.0;
+  vector<int> changes(ptr->maxVal, 0);
+  vector<int> exchanges(ptr->maxVal, 0);
+  int changeAmount; //each of the change amout that neet to get the number of coins
+  int bestExactChange; //minimized number of coins of each changeAmount
+  int eachDenom; //each denomination(or coin) in the given Denoms vector
+  int amountDiff; //definition: changeAmount-eachDenom
+  float finalScore = 0; //the sum number of changeArray, with panality(N) considered
+
+  //get changeArray with given denoms vector
+  for(int idx=0; idx<changes.size(); idx++) {
+    changeAmount = idx + 1;
+    bestExactChange = INT_MAX;
+    if (changeAmount == 1) {
+      changes[idx] = 1;
+    } else {
+      for (set<int>::iterator it = denoms.begin(); it != denoms.end(); it++) {
+        eachDenom = *it;
+        amountDiff = changeAmount - eachDenom;
+        if (amountDiff > 0) {
+          bestExactChange = min(bestExactChange, changes[amountDiff-1] + 1);
+        } else if (amountDiff == 0) {
+          bestExactChange = 1;
+        }
+      }
+      changes[idx] = bestExactChange;
+    }
+  }
+
+  for (int idx = 0; idx < changes.size(); idx++) {
+    int exceedAmount;
+    int amount = idx + 1;
+    int minExchange = changes[idx];
+
+    for (set<int>::iterator it = denoms.begin(); it != denoms.end(); it++) {
+      eachDenom = *it;
+      if (amount % eachDenom == 0) {
+        exceedAmount = (amount/eachDenom) * eachDenom;
+      } else {
+        exceedAmount = (int(amount/eachDenom)+1) * eachDenom;
+      }
+
+      if (exceedAmount < ptr->maxVal && exceedAmount - amount > 0) {
+        minExchange = min(minExchange, changes[exceedAmount-1] + changes[exceedAmount-amount-1]);
+      } else if (amount == ptr->maxVal) {
+        minExchange = min(minExchange, changes[0]);
+      } else {
+        continue;
+      }
+    }
+    exchanges[idx] = minExchange;
+  }
+
+  for (int idx = 0; idx < exchanges.size(); idx++) {
+    if ((idx + 1) % 5 == 0) {
+      finalScore += exchanges[idx] * ptr->N;
+    } else {
+      finalScore += exchanges[idx];
+    }
+  }
+
+  return finalScore;
 }
 
 int main(int argc, char **argv) {
