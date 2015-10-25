c0db7217b0d7850334876bdf2312f05e368477b4
GabrielWen <gabriel.weng@gmail.com>
2015-10-25 00:23:31 -0400

    Fix: Integrate genVoronoi into player.py

--- gravoronoi/player.py
+++ gravoronoi/player.py
@@ -20,7 +20,7 @@ class Player(Client):
     self.points.fill(-1)
     self.colors = np.zeros([2, 3], dtype=np.uint8) #Dummy data to run score script
     self.playerIdx = idx
-    self.oppIdx = idx ^ 0
+    self.oppIdx = idx ^ 1
     self.board = np.zeros([1000, 1000], dtype=np.uint8)
     self.board.fill(-1)
     self.moveNum = 0
@@ -29,72 +29,78 @@ class Player(Client):
     '''
     Return tuple of player scores, first element is our score, second one is opponent's
     '''
-    def get_score(self):
-      scores = genVoronoi.get_scores(2)
-      return scores[self.playerIdx], scores[self.oppIdx]
+  def get_score(self):
+    scores = genVoronoi.get_scores(2)
+    return scores[self.playerIdx], scores[self.oppIdx]
 
+  '''
+  Calculate score of a given move
+  '''
+  def probe_score(self, moveNum, player, x, y):
+    self.points[player][moveNum][0] = x
+    self.points[player][moveNum][1] = y
+    genVoronoi.generate_voronoi_diagram(2, self.numMoves, self.points, self.colors, None, 0, 0)
+    scores = self.get_score()
+
+    #undo
+    self.points[player][moveNum][0] = -1
+    self.points[player][moveNum][1] = -1
+    genVoronoi.generate_voronoi_diagram(2, self.numMoves, self.points, self.colors, None, 0, 0)
+
+    return scores
+
+  def validMove(self, x, y):
+    return self.board[x][y] == -1
+
+  def makeMove(self):
+    move = self.make_random_move()
     '''
-    Calculate score of a given move
-    '''
-    def probe_score(self, moveNum, player, x, y):
-      self.points[player][moveNum][0] = x
-      self.points[player][moveNum][1] = y
-      genVoronoi.generate_voronoi_diagram(2, self.numMoves, self.points, self.colors, None, 0, 0)
-      scores = self.get_score()
-
-      #undo
-      self.points[player][moveNum][0] = -1
-      self.points[player][moveNum][1] = -1
-      genVoronoi.generate_voronoi_diagram(2, self.numMoves, self.points, self.colors, None, 0, 0)
-
-      return scores
-
-    def validMove(self, x, y):
-      return self.board[x][y] == -1
-
-    '''
-    TODO: Update this method should do the job
-    def make_random_moves(self):
-      pass
+    self.updatePoints(self.playerIdx, move[0], move[1])
+    genVoronoi.generate_voronoi_diagram(2, self.numMoves, self.points, self.colors, None, 0, 0)
+    print 'Current score: {0}'.format(self.get_score())
     '''
 
-    def reset(self):
-      Client.reset(self)
-      self.points.fill(-1)
-      self.board.fill(-1)
-      self.moveNum = 0
-      self.moves = 0
+    return move
 
-    def updateMoveNum(self):
-      if self.moves % 2 == 0:
-        self.moveNum += 1
+  def reset(self):
+    Client.reset(self)
+    self.points.fill(-1)
+    self.board.fill(-1)
+    self.moveNum = 0
+    self.moves = 0
 
-    def dataReceived(self, data):
-      print 'Player {0} Received: {1}'.format(self.name, data)
-      line = data.strip()
-      items = line.split('\n')
-      if items[-1] == 'TEAM':
-        self.transport.write(self.name)
-      elif items[-1] == 'MOVE':
-        if items[0] == 'RESTART':
-          self.reset()
-          del items[0]
+  def updateMoveNum(self):
+    if self.moves % 2 == 0:
+      self.moveNum += 1
 
-        for item in items[:-1]:
-          parts = item.split()
-          x, y = int(parts[1]), int(parts[2])
-          self.points[self.oppIdx][self.moveNum][0] = x
-          self.points[self.oppIdx][self.moveNum][1] = y
-          self.board[x][y] = self.oppIdx
-          self.moves += 1
+  def updatePoints(self, player, x, y):
+    self.points[player][self.moveNum][0] = x
+    self.points[player][self.moveNum][1] = y
+    self.board[x][y] = player
 
-        self.updateMoveNum()
-        move = self.make_random_move()
-        print 'Player {1} making move: {1}'.format(move)
-        self.transport.write('{0} {1}'.format(move[0], move[1])
+  def dataReceived(self, data):
+    print 'Player {0} Received: {1}'.format(self.name, data)
+    line = data.strip()
+    items = line.split('\n')
+    if items[-1] == 'TEAM':
+      self.transport.write(self.name)
+    elif items[-1] == 'MOVE':
+      if items[0] == 'RESTART':
+        self.reset()
+        del items[0]
+
+      for item in items[:-1]:
+        parts = item.split()
+        x, y = int(parts[1]), int(parts[2])
+        self.updatePoints(self.oppIdx, x, y)
         self.moves += 1
-        self.updateMoveNum()
 
+      self.updateMoveNum()
+      move = self.makeMove()
+      print 'Player {0} making move: {1}'.format(self.name, move)
+      self.transport.write('{0} {1}'.format(move[0], move[1]))
+      self.moves += 1
+      self.updateMoveNum()
 
 class PlayerFactory(ClientFactory):
   def __init__(self, name, idx, numMoves):
