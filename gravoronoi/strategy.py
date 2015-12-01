b20a582a3883af49d1f6e673e7ed0f0cc4aa8c5d
GabrielWen <gabriel.weng@gmail.com>
2015-11-30 21:15:42 -0500

    Add: new strategy - skeleton

---
+++ gravoronoi/strategy.py
@@ -0,0 +1,69 @@
+import argparse
+import random
+import numpy as np
+import datetime
+import math
+
+from twisted.internet import reactor, protocol
+
+import genVoronoi
+from client import Client, ClientFactory
+
+class Player(Client):
+  def __init__(self, name, numMoves):
+    Client.__init__(self, name)
+    self.grid = np.zeros((1000, 1000))
+    self.oppMoves = []
+    self.myMoves = []
+
+  def dataReceived(self, data):
+    print 'Player {0} Received: {1}'.format(self.name, data)
+    line = data.strip()
+    items = line.split('\n')
+    if items[-1] == 'TEAM':
+      self.transport.write(self.name)
+      return
+    if items[-1] == 'END':
+      return
+
+    assert items[-1] == 'MOVE'
+
+    if len(items) == 1:
+      self.writeMove((500, 500))
+
+    for item in items[:-1]:
+      parts = item.split()
+      player, x, y = parts[0], int(parts[1]), int(parts[2])
+    move = self.make_random_move()
+    self.writeMove(move)
+
+  def writeMove(self, move):
+    self.myMoves.append(move)
+    self.transport.write('{0} {1}'.format(move[0], move[1]))
+
+class PlayerFactory(ClientFactory):
+  def __init__(self, name, numMoves):
+    ClientFactory.__init__(self, name)
+    self.numMoves = numMoves
+
+  def buildProtocol(self, addr):
+    c = Player(self.name, self.numMoves)
+    c.addr = addr
+    return c
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('name', help='Team Name')
+  parser.add_argument('-p', '--port', type=int, default=1337, help='Server port')
+  parser.add_argument('-m', '--numMoves', type=int, default=10, help='Number of moves available')
+  args = parser.parse_args()
+
+  name = args.name
+  port = args.port
+  numMoves = args.numMoves
+  factory = PlayerFactory(name, numMoves)
+  reactor.connectTCP('127.0.0.1', port, factory)
+  reactor.run()
+
+if __name__ == '__main__':
+  main()
