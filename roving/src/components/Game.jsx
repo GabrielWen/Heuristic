cfc48a53333a01ed6c9ffff65b711e01e6734989
GabrielWen <gabriel.weng@gmail.com>
2015-12-11 18:34:10 -0500

    Modulize

--- roving/src/components/Game.jsx
+++ roving/src/components/Game.jsx
@@ -1,13 +1,19 @@
 'use strict';
 
 var _lo = require('lodash');
+var util = require('util');
 var React = require('react');
 var Input = require('react-bootstrap').Input;
 var ButtonInput = require('react-bootstrap').ButtonInput;
+var Button = require('react-bootstrap').Button;
+var ButtonGroup = require('react-bootstrap').ButtonGroup;
+var Panel = require('react-bootstrap').Panel;
 
 var constants = require('../common/constants');
 var GameActions = require('../actions/GameActions');
 var GameStores = require('../stores/GameStores');
+
+var SettingForm = require('../common/SettingForm.jsx');
 
 var Game = React.createClass({
   getInitialState: function() {
@@ -23,37 +29,28 @@ var Game = React.createClass({
   _onChange: function() {
     this.setState(GameStores.getState());
   },
-  render: function() {
-    console.log(this.state.grid);
-    return (
-      <SettingForm handleSubmit={GameActions.handleGameInit}/>
-    );
-  }
-});
-
-var SettingForm = React.createClass({
-  onSubmit: function() {
-    var numRows = React.findDOMNode(this.refs.numRows).value;
-    this.props.handleSubmit({
-      numRows: parseInt(React.findDOMNode(this.refs.numRows).value),
-      numCols: parseInt(React.findDOMNode(this.refs.numCols).value),
-      numBombs: parseInt(React.findDOMNode(this.refs.numBombs).value),
-      numRovers: parseInt(React.findDOMNode(this.refs.numRovers).value)
-    });
+  test: function() {
+    console.log('Game TEST');
   },
   render: function() {
+    var title = this.state.gameInit ? util.format('Game Playing: %s x %s with %s bombs and %s rovers',
+                                      this.state.gameConfig.numRows, this.state.gameConfig.numCols,
+                                      this.state.gameConfig.numBombs, this.state.gameConfig.numRovers) :
+                                      'Please choose configurations';
+
+    //TODO: onClick here
+    var button = this.state.gameInit ? (
+      <ButtonGroup>
+        <Button bsStyle="primary">Play</Button>
+        <Button bsSylte="danger">Reset</Button>
+      </ButtonGroup>
+    ) : null;
+
     return (
-      <form className="form-inline">
-        <label>Num Rows</label>
-        <input type="number" ref="numRows" defaultValue={constants.DefaultSetting.numRows}/>
-        <label>Num Cols</label>
-        <input type="number" ref="numCols" defaultValue={constants.DefaultSetting.numCols}/>
-        <label>Num Bombs</label>
-        <input type="number" label="Num Bombs" ref="numBombs" defaultValue={constants.DefaultSetting.numBombs}/>
-        <label>Num Rovers</label>
-        <input type="number" label="Num Rovers" ref="numRovers" defaultValue={constants.DefaultSetting.numRovers}/>
-        <ButtonInput type="submit" onClick={this.onSubmit}/>
-      </form>
+      <Panel header={title}>
+        {this.state.gameInit ? null : <SettingForm handleSubmit={GameActions.handleGameInit}/>}
+        {button}
+      </Panel>
     );
   }
 });
