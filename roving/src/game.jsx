942598feba435ba346921c1977f2fb0a745e0569
GabrielWen <gabriel.weng@gmail.com>
2015-12-11 17:28:43 -0500

    Undo

--- roving/src/game.jsx
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _lo = require('lodash');
-var React = require('react');
-
-var Grid = require('./components/Grid.jsx');
-var Game = require('./components/Game.jsx');
-
-React.render(<Game/>, document.getElementById('gameArea'));
