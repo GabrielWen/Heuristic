8d48cc98a3f1ac6f89e9156d9e8df75abbef54f9
jfriend08 <ys486@cornell.edu>
2015-12-12 14:50:23 -0500

    add burst sound

--- roving/src/stores/GameStores.js
+++ roving/src/stores/GameStores.js
@@ -220,12 +220,14 @@ var GameStores = BaseStore.createStore({
       case constants.State.BOMB:
         //TODO: Add lose logic
         this.grid[v[0]][v[1]] = constants.State.BURST;
+        play(constants.Sounds.explosion).autoplay();
         break;
       case constants.State.DEST:
         //TODO: Add win logic
         break;
       case constants.State.BURST:
         this.grid[v[0]][v[1]] = constants.State.PLAYER_ON_BURST;
+        play(constants.Sounds.explosion).autoplay();
         break;
       default:
         this.grid[v[0]][v[1]] = constants.State.PLAYER;
